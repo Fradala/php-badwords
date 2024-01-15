3048ea30019b817339b8389dab3868601c25c5db
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-01-15 13:26:40 +0100

    mod

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+
+    $testo = "la nonna va al mare con il nonno"
+
+
+    $badword = $_GET['badword'];
+
+    $censoredParagraph = str_ireplace($badword, " *** ", $paragraph);
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,21 +26,36 @@
         parole
     </h1>
 
-    <form action="./server.php" method="POST">
+    <form action="./index.php" method="GET">
         <label for="text">Inserici parola da eliminare</label>
         <input type="text" name="parola" id="parola">
+        <button type="submit">censura</button>
     </form>
+
+    <h2>
+        testo: <br>
+        lunghezza: <?php echo strlen($testo); ?>
+    </h2>
+
+    <p>
+        <?php echo $testo; ?>
+    </p>
+
+    <hr>
+    <hr>
+
+    <h2>
+        Testo censurato: <br>
+        Lunghezza: <?php echo strlen($paroleDaCensurare); ?>
+    </h2>
+    <p>
+        <?php echo $paroleDaCensurare; ?>
+    </p>
+
 
     
 
-   <?php
-
-        
-        echo $testo="la barca di mia nonna <br>"; 
-        echo "Lunghezza: " . strlen($testo) . " caratteri";
-       
-       
-   ?>
+   
    
 
    
